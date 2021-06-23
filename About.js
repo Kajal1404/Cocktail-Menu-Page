1374a0697d0d462ee650feedaa9ed49e7c998f0f
Kajal1404 <84250721+Kajal1404@users.noreply.github.com>
2021-06-22 21:55:35 -0600

    Add files via upload

---
+++ About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        CocktailDB is one of the leading franchices selling branded cocktails
+        since 1996. One of the most authentic places where you can find delicious 
+        cocktails served with munchies and freshely prepared food.
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
+
+const Navbar = () => {
+  return (
+    <nav className ="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo"
+             className="logo"></img>
+        </Link>
+<ul classname='nav-links'>
+
+  <Link to='/'>
+    Home 
+  </Link > &nbsp;
+   <Link to='/about'>
+    About
+  </Link>
+</ul>
+      </div> 
+    </nav>
+  )
+}
+
+export default Navbar
